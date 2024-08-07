c73c2f243138c2025755c5ca74d516a3c03b335c
OneCDOnly <one.cd.only@gmail.com>
2024-08-07 14:00:15 +1000

    general code refresh - update actions for compatibility with sherpa, - improve message format.

--- shared/hidethatbanner.sh
+++ shared/hidethatbanner.sh
@@ -1,54 +1,128 @@
 #!/usr/bin/env bash
 ###############################################################################
-# hidethatbanner.sh - (C)opyright 2018-2024 OneCD - one.cd.only@gmail.com
-
+# hidethatbanner.sh
+# 	copyright 2018-2024 OneCD
+#
+# Contact:
+#	one.cd.only@gmail.com
+#
 # This script is part of the 'HideThatBanner' package
-
-# For more info: https://forum.qnap.com/viewtopic.php?f=320&t=140215
-
+#
 # Available in the MyQNAP store: https://www.myqnap.org/product/hidethatbanner
 # Project source: https://github.com/OneCDOnly/HideThatBanner
-
+# Community forum: https://forum.qnap.com/viewtopic.php?t=140215
+#
 # This program is free software: you can redistribute it and/or modify it under
 # the terms of the GNU General Public License as published by the Free Software
 # Foundation, either version 3 of the License, or (at your option) any later
 # version.
-
+#
 # This program is distributed in the hope that it will be useful, but WITHOUT
 # ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
 # FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
 # details.
-
+#
 # You should have received a copy of the GNU General Public License along with
 # this program. If not, see http://www.gnu.org/licenses/.
 ###############################################################################
+
+set -o nounset -o pipefail
+shopt -s extglob
+ln -fns /proc/self/fd /dev/fd		# KLUDGE: `/dev/fd` isn't always created by QTS.
 
 readonly USER_ARGS_RAW=$*
 
 Init()
     {
 
-	# KLUDGE: `/dev/fd` isn't always created by QTS.
-	ln -fns /proc/self/fd /dev/fd
-
     readonly QPKG_NAME=HideThatBanner
-        BUILD=$(/sbin/getcfg $QPKG_NAME Build -f /etc/config/qpkg.conf)
-        readonly SERVICE_STATUS_PATHFILE=/var/run/$QPKG_NAME.last.operation
-    NAS_FIRMWARE=$(/sbin/getcfg System Version -f /etc/config/uLinux.conf)
+
+    readonly NAS_FIRMWARE=$(/sbin/getcfg System Version -f /etc/config/uLinux.conf)
+    readonly QPKG_VERSION=$(/sbin/getcfg $QPKG_NAME Version -f /etc/config/qpkg.conf)
+	readonly SERVICE_ACTION_PATHFILE=/var/log/$QPKG_NAME.action
+	readonly SERVICE_RESULT_PATHFILE=/var/log/$QPKG_NAME.result
     readonly SOURCE_PATHFILE=/home/httpd/cgi-bin/apps/qpkg/css/qpkg.css
         readonly BACKUP_PATHFILE=${SOURCE_PATHFILE}.bak
-
-    readonly BUILD
-    readonly NAS_FIRMWARE
 
     /sbin/setcfg "$QPKG_NAME" Status complete -f /etc/config/qpkg.conf
 
     # KLUDGE: 'clean' the QTS 4.5.1 App Center notifier status.
     [[ -e /sbin/qpkg_cli ]] && /sbin/qpkg_cli --clean "$QPKG_NAME" > /dev/null 2>&1
 
-    [[ ${#USER_ARGS_RAW} -eq 0 ]] && echo "$QPKG_NAME ($BUILD)"
+    }
+
+StartQPKG()
+    {
+
+    [[ ! -e $BACKUP_PATHFILE ]] && cp "$SOURCE_PATHFILE" "$BACKUP_PATHFILE"
+
+    if [[ ${NAS_FIRMWARE//.} -lt 451 ]]; then
+        /bin/sed -i 's|.store_banner_area{|.store_banner_area{display:none;|' "$SOURCE_PATHFILE"
+    elif [[ ${NAS_FIRMWARE//.} -lt 500 ]]; then
+        /bin/sed -i 's|.store_banner_area,.banner_area{|.store_banner_area,.banner_area{display:none;|' "$SOURCE_PATHFILE"
+    else
+        /bin/sed -i 's|.store_banner_area,.banner_area{|.store_banner_area,.banner_area{display:none;|' "$SOURCE_PATHFILE"
+        /bin/sed -i 's| .banner_show{| .banner_show{display:none;|' "$SOURCE_PATHFILE"
+    fi
+
+    if ! (/bin/cmp -s "$SOURCE_PATHFILE" "$BACKUP_PATHFILE"); then
+        LogWrite 'App Center UI was patched successfully.' 0
+        return 0
+    else
+        LogWrite "App Center UI was not patched ($(GetQnapOS) $NAS_FIRMWARE)." 2
+        return 1
+    fi
 
     }
+
+StopQPKG()
+    {
+
+    [[ -e $BACKUP_PATHFILE ]] && mv "$BACKUP_PATHFILE" "$SOURCE_PATHFILE"
+    return 0
+
+    }
+
+StatusQPKG()
+    {
+
+    if [[ -e $BACKUP_PATHFILE ]] && ! (/bin/cmp -s "$SOURCE_PATHFILE" "$BACKUP_PATHFILE"); then
+        echo active
+        exit 0
+    else
+        echo inactive
+        exit 1
+    fi
+
+    }
+
+ShowTitle()
+    {
+
+    echo "$(ShowAsTitleName) $(ShowAsVersion)"
+
+    }
+
+ShowAsTitleName()
+	{
+
+	TextBrightWhite $QPKG_NAME
+
+	}
+
+ShowAsVersion()
+	{
+
+	printf '%s' "v$QPKG_VERSION"
+
+	}
+
+ShowAsUsage()
+    {
+
+    echo -e "\nUsage: $0 {start|stop|restart|status}"
+
+	}
 
 LogWrite()
     {
@@ -63,29 +137,6 @@ LogWrite()
 
     }
 
-SetServiceOperationResultOK()
-    {
-
-    SetServiceOperationResult ok
-
-    }
-
-SetServiceOperationResultFailed()
-    {
-
-    SetServiceOperationResult failed
-
-    }
-
-SetServiceOperationResult()
-    {
-
-    # $1 = result of operation to recorded
-
-    [[ -n $1 && -n $SERVICE_STATUS_PATHFILE ]] && echo "$1" > "$SERVICE_STATUS_PATHFILE"
-
-    }
-
 GetQnapOS()
 	{
 
@@ -97,46 +148,102 @@ GetQnapOS()
 
 	}
 
+SetServiceAction()
+	{
+
+	service_action=${1:-none}
+	CommitServiceAction
+	SetServiceResultAsInProgress
+
+	}
+
+SetServiceResultAsOK()
+	{
+
+	service_result=ok
+	CommitServiceResult
+
+	}
+
+SetServiceResultAsFailed()
+	{
+
+	service_result=failed
+	CommitServiceResult
+
+	}
+
+SetServiceResultAsInProgress()
+	{
+
+	# Selected action is in-progress and hasn't generated a result yet.
+
+	service_result=in-progress
+	CommitServiceResult
+
+	}
+
+CommitServiceAction()
+	{
+
+    echo "$service_action" > "$SERVICE_ACTION_PATHFILE"
+
+	}
+
+CommitServiceResult()
+	{
+
+    echo "$service_result" > "$SERVICE_RESULT_PATHFILE"
+
+	}
+
+TextBrightWhite()
+	{
+
+	[[ -n ${1:-} ]] || return
+
+    printf '\033[1;97m%s\033[0m' "$1"
+
+	}
+
 Init
 
-case "$1" in
-    start)
-        [[ ! -e $BACKUP_PATHFILE ]] && cp "$SOURCE_PATHFILE" "$BACKUP_PATHFILE"
+user_arg=${USER_ARGS_RAW%% *}		# Only process first argument.
 
-        if [[ ${NAS_FIRMWARE//.} -lt 451 ]]; then
-            /bin/sed -i 's|.store_banner_area{|.store_banner_area{display:none;|' "$SOURCE_PATHFILE"
-        elif [[ ${NAS_FIRMWARE//.} -lt 500 ]]; then
-            /bin/sed -i 's|.store_banner_area,.banner_area{|.store_banner_area,.banner_area{display:none;|' "$SOURCE_PATHFILE"
-        else
-            /bin/sed -i 's|.store_banner_area,.banner_area{|.store_banner_area,.banner_area{display:none;|' "$SOURCE_PATHFILE"
-            /bin/sed -i 's| .banner_show{| .banner_show{display:none;|' "$SOURCE_PATHFILE"
-        fi
+case $user_arg in
+    ?(--)restart)
+        SetServiceAction restart
 
-        if ! (/bin/cmp -s "$SOURCE_PATHFILE" "$BACKUP_PATHFILE"); then
-            LogWrite 'App Center UI was patched successfully.' 0
-            SetServiceOperationResultOK
+        if StopQPKG && StartQPKG; then
+            SetServiceResultAsOK
         else
-            LogWrite "App Center UI was not patched ($(GetQnapOS) $NAS_FIRMWARE)." 2
-            SetServiceOperationResultFailed
+            SetServiceResultAsFailed
         fi
         ;;
-    stop)
-        [[ -e $BACKUP_PATHFILE ]] && cp "$BACKUP_PATHFILE" "$SOURCE_PATHFILE"
-        SetServiceOperationResultOK
+    ?(--)start)
+        SetServiceAction start
+
+        if StartQPKG; then
+            SetServiceResultAsOK
+        else
+            SetServiceResultAsFailed
+        fi
         ;;
-    restart)
-        $0 stop
-        $0 start
-        ;;
-	status)
-        if ! (/bin/cmp -s "$SOURCE_PATHFILE" "$BACKUP_PATHFILE"); then
-			echo 'active'
-			exit 0
-		else
-			echo 'inactive'
-			exit 1
-		fi
+    ?(-)s|?(--)status)
+        StatusQPKG
 		;;
+    ?(--)stop)
+        SetServiceAction stop
+
+        if StopQPKG; then
+            SetServiceResultAsOK
+        else
+            SetServiceResultAsFailed
+        fi
+        ;;
     *)
-        echo "run as: $0 {start|stop|restart|status}"
+        ShowTitle
+        ShowAsUsage
 esac
+
+exit 0
